4240b631c15dba31d0cccc6426b48c262b9c79fa
DiogoAzvd <122066231+DiogoAzvd@users.noreply.github.com>
2026-03-31 13:49:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ function main() {
     }
 
     const app = new App();
+
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", "M 7.2 13.5 L 7.2 5.5 L 6.4 5.5 L 8 2.5 L 9.6 5.5 L 8.8 5.5 L 8.8 13.5 Z");
+    path.setAttribute("fill", "#000000");
+    path.classList.add("element", "svg-element", "editing");
+
+    app.svgWrapper.layer.importElement(path);
 }
 
 main();
